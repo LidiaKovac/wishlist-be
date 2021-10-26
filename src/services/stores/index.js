42a29191425c16fa0f6016369752f641dd00a526
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-10-26 20:47:12 +0200

    adds working store recognition and image scraping
    Added Shein, About You and Asos as stores

--- src/services/stores/index.js
+++ src/services/stores/index.js
@@ -18,13 +18,14 @@ store_route.get(
         .get("https://www." + prod)
         .then(async (page) => {
           let stringHTML = page.data;
+          let product = await Product.findOne({ name: prod.split("/")[3] });
+          //checks if product exists
+          if (!product) {
+            console.log("creating new prod");
+            let newProd = await Product.create({ name: prod.split("/")[3], url: "http://" + prod, author: "6171d52268b69dc1059f2d55" });
+            console.log("created", newProd);
+          }
           if (detectedStore === "asos") {
-            let product = await Product.findOne({ name: prod.split("/")[3] });
-            if (!product) {
-              console.log("creating new prod");
-              let newProd = await Product.create({ name: prod.split("/")[3], url: "http://" + prod, author: "6171d52268b69dc1059f2d55" });
-              console.log("created", newProd);
-            }
             const baseUrl = "https://images.asos-media.com/products/";
             //     console.log(url);
             console.log(prod);
@@ -37,22 +38,20 @@ store_route.get(
           }
           if (detectedStore === "aboutyou") {
             //aboutyou.it/p/max-co/abito-camicia-tritare-7041743
-            let product = await Product.findOne({ name: prod.split("/")[3] });
-            if (product) {
-              let trim = stringHTML.split('<script type="application/ld+json">');
-              //console.log(trim[2]);
-              let imgArr = trim[2].split('"image":["');
-              //console.log(imgArr[1]);
-              let img = imgArr[1].split('","');
-              //console.log(img[0]);
-              res.send({ image: img, store: "About you" });
-            }
+            //https://cdn.aboutstatic.com/file/6712e6159b51d509930a05318f22fce0.jpg
+            // let product = await Product.findOne({ name: prod.split("/")[3] });
+            let JSONFromHTML = stringHTML.split('<script type="application/ld+json">')[2] //imageURL is here 
+            let imageUrl = JSONFromHTML.split(`"image":["`)[1].split("?")[0] 
+            
+            res.send({ image: imageUrl, store: "About you", author: "6171d52268b69dc1059f2d55" });
+            
           }
           if (detectedStore === "shein") {
             let meta = stringHTML.split('<meta property="og:image" content="');
-            let imageLink = meta[1].split('">')[0];
+            let imageLink = meta[1].split('">')[0].split(".jpg")[0]
+            console.log(imageLink);
             //console.log(imageLink);
-            res.status(200).send({ image: "https://" + imageLink, store: "Shein" });
+            res.status(200).send({ image: "https://" + imageLink.split(".jpg")[0] + ".jpg", store: "Shein", author: "6171d52268b69dc1059f2d55" });
           }
           if (detectedStore !== "shein" && detectedStore !== "aboutyou" && detectedStore !== "asos") {
             throw "Store does not exist";
